368ae7766203aefaa77f8ccbbe817ee8b898ebb7
Justin Kauszler <jkauszler@gmail.com>
2019-11-22 15:52:01 -0500

    step: 0 - init

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,20 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+const element = {
+  type: "h1",
+  props: {
+    title: "foo",
+    children: "Hello World",
+  },
+};
+
+const container = document.getElementById("root");
+
+const node =document.createElement(element.type);
+node["title"] = element.props.title;
+
+const text = document.createTextNode("");
+text["nodeValue"] = element.props.children;
+
+node.appendChild(text);
+container.appendChild(node);
